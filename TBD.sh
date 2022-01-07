f70bc172ba89637154fd5a1524a4b3d8df124536
HikawaRin <HikawaRin@outlook.com>
2022-01-07 01:04:37 -0500

    fix: rename tTBD to iTBD

--- TBD.sh
+++ TBD.sh
@@ -3,7 +3,7 @@
 function build_package {
 	echo "Building file [$1]..."
 	source $1
-	docker run -i -v $(realpath $1):/src/pkgmeta -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/tTBD.sh:/usr/bin/tTBD.sh -v $PWD:/publish $image "bash" "-c" "source /src/pkgmeta && source /usr/bin/tTBD.sh"
+	docker run -i -v $(realpath $1):/src/pkgmeta -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/iTBD.sh:/usr/bin/iTBD.sh -v $PWD:/publish $image "bash" "-c" "source /src/pkgmeta && source /usr/bin/iTBD.sh"
 }
 
 if [[ $# -eq 0 ]] ; then
