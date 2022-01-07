8e066781348fcd220fb6a11e04850f24683973e2
CI <circle@ci.com>
2022-01-07 10:59:19 +0000

    Circle CI: Deploy

---
+++ TBD.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function build_package {
+	echo "Building file [$1]..."
+	source $1
+	docker run -i -v $(realpath $1):/src/pkgmeta -v $(dirname "$(realpath "${BASH_SOURCE[0]}")")/iTBD.sh:/usr/bin/iTBD.sh -v $PWD:/publish $image "bash" "-c" "source /src/pkgmeta && source /usr/bin/iTBD.sh"
+}
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Please specify a filename or `all` for all .TBD files.'
+    exit 0
+fi
+
+if [ $1 = "all" ]; then
+   for tbdfile in *.TBD; do
+	   build_package $tbdfile
+   done
+else
+   build_package $1
+fi
