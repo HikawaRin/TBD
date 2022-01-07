662aae243c87c83482c9945d0e628e9121407eb3
HikawaRin <HikawaRin@outlook.com>
2022-01-07 05:58:21 -0500

    hope last

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@ echo $1
 git config --global user.email "circle@ci.com"
 git config --global user.name "CI"
 
-git checkout --orphan circle_ci_release
+git checkout --orphan circle_ci_release_test
+git remote add release https://${1}@github.com/HikawaRin/TBD.git
+git pull
+
 git add results
 git commit -am "Circle CI: Deploy"
-git remote add release https://${1}@github.com/HikawaRin/TBD.git
-git push release
 
+git push release
