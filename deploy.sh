452dc5aa9a43422db050352918f896e860ad2ed4
HikawaRin <HikawaRin@outlook.com>
2022-01-07 03:53:12 -0500

    config git

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo ${GITHUB_TOKEN}
+git config --global user.email "circle@ci.com"
+git config --global user.name "CI"
+
 git add results
 git commit -am "Circle CI: auto append"
 git checkout -b circle_ci_release
-echo ${GITHUB_TOKEN}
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/HikawaRin/TBD.git
 git push origin
 
