71e84c7cbbb2dce1b55f1161c255fb21f3741f90
HikawaRin <HikawaRin@outlook.com>
2022-01-07 03:41:02 -0500

    move deploy.sh to home

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 git add results
 git commit -am "Circle CI: auto append"
 git checkout -b circle_ci_release
+echo ${GITHUB_TOKEN}
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/HikawaRin/TBD.git
 git push origin
 
