c43174afcfe6f0a628da87d53c2f1b5b0c5d30a7
HikawaRin <HikawaRin@outlook.com>
2022-01-07 05:53:58 -0500

    last

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,9 @@ echo $1
 git config --global user.email "circle@ci.com"
 git config --global user.name "CI"
 
-ls
-git status
+git checkout --orphan circle_ci_release
 git add results
-git commit -am "Circle CI: auto append"
-git checkout -b circle_ci_release
-ls
+git commit -am "Circle CI: Deploy"
 git remote add release https://${1}@github.com/HikawaRin/TBD.git
 git push release
 
