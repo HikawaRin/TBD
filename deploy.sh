37eecaf1f14369051e9960251c2e84dad7a1754a
HikawaRin <HikawaRin@outlook.com>
2022-01-07 06:11:15 -0500

    force

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,10 @@ echo $1
 git config --global user.email "circle@ci.com"
 git config --global user.name "CI"
 
-git checkout --orphan circle_ci_release_test
+git checkout --orphan circle_ci
 git remote add release https://${1}@github.com/HikawaRin/TBD.git
-git pull release
 
 git add results
 git commit -am "Circle CI: Deploy"
 
-git push release
+git push --force release
