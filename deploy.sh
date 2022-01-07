f2325af2f28b11c8f5bc562d65b56fa2edfa089e
HikawaRin <HikawaRin@outlook.com>
2022-01-07 06:04:23 -0500

    git pull release

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git config --global user.name "CI"
 
 git checkout --orphan circle_ci_release_test
 git remote add release https://${1}@github.com/HikawaRin/TBD.git
-git pull
+git pull release
 
 git add results
 git commit -am "Circle CI: Deploy"
